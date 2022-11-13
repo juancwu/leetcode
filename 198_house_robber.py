3f31d24aa589f4b4f58400aca0a0c8b9f7b76b3a
juancwu <jc@macbook.com>
2022-11-13 01:57:25 -0500

    added dp method to question 198

--- 198_house_robber.py
+++ 198_house_robber.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
         """
@@ -24,3 +26,26 @@ class Solution:
         self.memo[i] = max(self.decideRob(i + 1), self.decideRob(i + 2) + self.nums[i])
         
         return self.memo[i]
+
+class Solution2:
+    def rob(self, nums: List[int]) -> int:
+        """
+        the robber cannot take from adjacent houses
+        this means that the relation is taking the best between
+        
+        dp[i] = max(dp[i + 1], dp[i + 2] + nums[i])
+        is robbing the next house yield greater profit than the current and next possible house
+        
+        use dynamic programming, tabulation
+        """
+        n = len(nums)
+        dp = [0] * (n + 1)
+        
+        # base
+        dp[n] = 0
+        dp[n - 1] = nums[n - 1]
+        
+        for i in range(n - 2, -1, -1):
+            dp[i] = max(dp[i + 1], dp[i + 2] + nums[i])
+        
+        return dp[0]
